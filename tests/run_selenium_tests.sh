11623b4c9e0c741503b4b71e243e81b1a56669bb
Gunes Acar <gunes.acar@esat.kuleuven.be>
2014-08-30 05:19:07 -0700

    Log browser version

--- tests/run_selenium_tests.sh
+++ tests/run_selenium_tests.sh
@@ -3,6 +3,16 @@ virtualenv PBTESTENV
 source PBTESTENV/bin/activate
 pip install -r sel_requirements.txt
 
+export VERSION=$(echo $BROWSER | sed -e's/[^-]*-//')
+export BROWSER=$(echo $BROWSER | sed -e's/-.*//')
+
+# https://github.com/PolymerLabs/polymerchromeapp/blob/master/components/web-animations-js/.travis-setup.sh
+echo BROWSER=$BROWSER
+echo VERSION=$VERSION
+echo "*************************"
+echo "Running $BROWSER $VERSION"
+echo "*************************"
+
 export ENABLE_XVFB=1    # run the tests headless using Xvfb. Set 0 to disable
 py.test -s selenium  # autodiscover and run the tests
 
