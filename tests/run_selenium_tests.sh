3eb814c32d863066081cfb191be014f629352f8a
Gunes Acar <gunes.acar@esat.kuleuven.be>
2015-06-12 19:41:09 +0200

    Install Chromium and print the version.

--- tests/run_selenium_tests.sh
+++ tests/run_selenium_tests.sh
@@ -4,5 +4,6 @@ source PBTESTENV/bin/activate
 pip install -r sel_requirements.txt
 
 export ENABLE_XVFB=1    # run the tests headless using Xvfb. Set 0 to disable
+chromium-browser --version
 py.test -s selenium  # autodiscover and run the tests
 
